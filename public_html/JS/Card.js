68c7993d30e7b6e42cc4aa74ada2859d2e081b17
KBaselSC <basel@sheridancollege.ca>
2020-02-09 14:36:35 -0500

    added variables, event listener on click for the generate invite button added a function to make the form vanish, and the card to appear.

--- public_html/JS/Card.js
+++ public_html/JS/Card.js
@@ -4,4 +4,16 @@
  * and open the template in the editor.
  */
 
+var name = document.getElementById("name").value;
+var color = document.getElementById("color").value;
+var cardCanvas = document.getElementById("cardCanvas");
+var subInv = document.getElementById("subInv");
+var formCont = document.getElementByClass("formCont");
 
+subInv.addEventListener("click", submitInv);
+
+function submitInv(){
+    formCont.style.display="none";
+    //function
+    cardCanvas.style.display="block";
+}
