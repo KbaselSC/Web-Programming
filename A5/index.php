12daab11cc7f9f9a3a1761fde67b5d99b0b889f8
KBaselSC <basel@sheridancollege.ca>
2020-04-03 23:54:32 -0400

    Added all the php and edited some stuff here and there

--- A5/index.php
+++ A5/index.php
@@ -12,7 +12,141 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        //Variables
+        $name = $_POST["name"];
+        $coffeeAmt = $_POST["coffeeAmt"];
+        $size = $_POST["size"];
+        $creamAmt = $_POST["creamAmt"];
+        $sugarAmt = $_POST["sugarAmt"];
+        echo "<h1> Thanks for ordering!</h1>";
+        
+        //cases for which size of coffee the person ordered as well as the amount
+        //small
+        if (($coffeeAmt == 1) && ($size == "sml")) {
+            echo "<img src=./Images/cupsml.jpg>";
+        } elseif (($coffeeAmt == 2) && ($size == "sml")) {
+            echo "<img src=./Images/cupsml.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupsml.jpg>";
+        } elseif (($coffeeAmt == 3) && ($size == "sml")) {
+            echo "<img src=./Images/cupsml.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupsml.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupsml.jpg>";
+        }
+        elseif(($coffeeAmt == 4) && ($size == "sml")){
+            echo "<img src=./Images/cupsml.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupsml.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupsml.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupsml.jpg>";
+        }
+        //medium
+        if (($coffeeAmt == 1) && ($size == "med")) {
+            echo "<img src=./Images/cupmed.jpg>";
+        } elseif (($coffeeAmt == 2) && ($size == "med")) {
+            echo "<img src=./Images/cupmed.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupmed.jpg>";
+        } elseif (($coffeeAmt == 3) && ($size == "med")) {
+            echo "<img src=./Images/cupmed.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupmed.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupmed.jpg>";
+        }
+        elseif(($coffeeAmt == 4) && ($size == "med")){
+            echo "<img src=./Images/cupmed.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupmed.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupmed.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cupmed.jpg>";
+        }
+        //large
+        if (($coffeeAmt == 1) && ($size == "lrg")) {
+            echo "<img src=./Images/cuplrg.jpg>";
+        } elseif (($coffeeAmt == 2) && ($size == "lrg")) {
+            echo "<img src=./Images/cuplrg.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cuplrg.jpg>";
+        } elseif (($coffeeAmt == 3) && ($size == "lrg")) {
+            echo "<img src=./Images/cuplrg.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cuplrg.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cuplrg.jpg>";
+        }
+        elseif(($coffeeAmt == 4) && ($size == "lrg")){
+            echo "<img src=./Images/cuplrg.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cuplrg.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cuplrg.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cuplrg.jpg>";
+        }
+        //extra large
+        if (($coffeeAmt == 1) && ($size == "xlrg")) {
+            echo "<img src=./Images/cup.jpg>";
+        } elseif (($coffeeAmt == 2) && ($size == "xlrg")) {
+            echo "<img src=./Images/cup.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cup.jpg>";
+        } elseif (($coffeeAmt == 3) && ($size == "xlrg")) {
+            echo "<img src=./Images/cup.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cup.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cup.jpg>";
+        }
+        elseif(($coffeeAmt == 4) && ($size == "xlrg")){
+            echo "<img src=./Images/cup.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cup.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cup.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cup.jpg>";
+        }
+        //cream
+        if ($creamAmt == 1) {
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cream.jpg>";
+        } elseif ($creamAmt == 2) {
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cream.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cream.jpg>";
+        } elseif ($creamAmt == 3) {
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cream.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cream.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/cream.jpg>";
+        }
+        //sugar
+        if ($sugarAmt == 1) {
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/sugar.jpg>";
+        } elseif ($sugarAmt == 2) {
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/sugar.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/sugar.jpg>";
+        } elseif ($sugarAmt == 3) {
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/sugar.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/sugar.jpg>";
+            echo "<img src=./Images/plus.jpg>";
+            echo "<img src=./Images/sugar.jpg>";
+        }
         ?>
     </body>
 </html>
